5d60cb2f74c831a93b22341d760e9b1934a4d015
egororo <egorsemirenko04@gmail.com>
2025-11-13 20:19:42 +0300

    fixed sample

--- samples/main.cu
+++ samples/main.cu
@@ -1,36 +1,21 @@
-#include <stdio.h>
-#include <math.h>
-#define N 1024
+#include <stdio.h> 
+#define N (1024*1024)  
 
-__global__ void kernel(float *dA) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < N) {
-        float x = 2.0f * 3.1415926f * (float)idx / (float)N;
-        dA[idx] = sinf(sqrtf(x));
-    }
-}
+__global__ void kernel ( float * dA )  { 
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;   
+    float x = 2.0f * 3.1415926f * (float) idx / (float) N;    
+    dA [idx] = sinf (sqrtf ( x ) ); 
+} 
+ 
 
-int main() {
-    float *hA, *dA;
-
-    hA = (float*)malloc(N * sizeof(float));
-    cudaMalloc(&dA, N * sizeof(float));
-
-    kernel<<<N/512, 512>>>(dA);
-
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        printf("KERNEL ERROR: %s\n", cudaGetErrorString(err));
-        return 1;
-    }
-
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(hA, dA, N * sizeof(float), cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i < 16; i++)
-        printf("a[%d] = %.8f\n", i, hA[i]);
-
-    free(hA);
-    cudaFree(dA);
-}
+int main ( int argc, char * argv [] )  {
+    float *hA, *dA;   
+    hA = ( float* ) malloc (N * sizeof ( float ) );   
+    cudaMalloc ( (void**)&dA, N * sizeof ( float ) );  
+    kernel <<< N/512, 512 >>> ( dA );   
+    cudaMemcpy ( hA, dA, N * sizeof ( float ), cudaMemcpyDeviceToHost );  
+    for ( int idx = 0; idx < N; idx++ ) printf ( "a[%d] = %.5f\n", idx, hA[idx] );   
+    free ( hA ); 
+    cudaFree ( dA );   
+    return 0; 
+} 
