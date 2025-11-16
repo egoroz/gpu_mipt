5c3b920ec3abc23b61231a17b0068a61c462b677
egororo <egorsemirenko04@gmail.com>
2025-11-16 20:17:01 +0300

    colab

--- samples/sum/padding.cu
+++ samples/sum/padding.cu
@@ -48,18 +48,18 @@ int main(int argc, char** argv){
     CUDA_CHECK(cudaEventCreate(&start_gpu));
     CUDA_CHECK(cudaEventCreate(&stop_gpu));
     
-    CUDA_CHECK(cudaEventRecord(start_gpu));  // start time GPU
     float *dA = nullptr, *dPartial = nullptr;
     CUDA_CHECK(cudaMalloc(&dA, paddedN * sizeof(float)));
     CUDA_CHECK(cudaMalloc(&dPartial, n_threads * n_blocks * sizeof(float)));
     CUDA_CHECK(cudaMemset(dPartial, 0, n_threads * n_blocks * sizeof(float)));
     CUDA_CHECK(cudaMemcpy(dA, hA, paddedN * sizeof(float), cudaMemcpyHostToDevice));
-
+    
+    CUDA_CHECK(cudaEventRecord(start_gpu));  // start time GPU
     reduceKernel<<<n_blocks, n_threads>>>(dA, dPartial, paddedN);
     CUDA_CHECK(cudaEventRecord(stop_gpu));  // end time GPU
-
+    
     CUDA_CHECK(cudaGetLastError());
-
+    
     CUDA_CHECK(cudaMemcpy(hPartial, dPartial, n_threads * n_blocks * sizeof(float), cudaMemcpyDeviceToHost));  
 
     float result = 0;
