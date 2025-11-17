1f0e784d36fd1a88a2aeaf67f442b19dd6535b17
egororo <egorsemirenko04@gmail.com>
2025-11-17 23:41:54 +0300

    enchanced readability

--- samples/sum/padding.cu
+++ samples/sum/padding.cu
@@ -18,8 +18,7 @@ void fill_array(size_t N, float* arr){
 }
 
 __global__ void reduceKernel(const float* __restrict__ dA, float* __restrict__ dPartial, size_t N){
-    size_t shift = blockDim.x * blockIdx.x;
-    int tid = threadIdx.x + shift;
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
     size_t row_sz = blockDim.x * gridDim.x;
     size_t rows = N / row_sz;
     
